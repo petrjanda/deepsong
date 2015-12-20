ff227b6b778973e2b0dcf1d968ee13fb7e5d8994
Petr Janda <petrjandacz@gmail.com>
2015-12-20 00:32:53 +0000

    adding more robust validation

--- train.lua
+++ train.lua
@@ -3,7 +3,7 @@ npy4th = require 'npy4th'
 
 -- TODO: move to conf
 --wide = 4096
-wide = 4096
+wide = 512
 
 -- input, model, training config
 local conf = {
@@ -17,12 +17,12 @@ local conf = {
   -- model
   m = {
     num_classes = 3,
-    num_hidden = 1000
+    num_hidden = 300
   },
 
   -- training
   t = {
-    epochs = 30
+    epochs = 75
   }
 }
 
@@ -33,9 +33,10 @@ yt = torch.Tensor(conf.i.num_samples)
 i = 1
 for a in paths.iterdirs(conf.i.path) do
     for s in paths.iterfiles(conf.i.path .. a) do
-        input = npy4th.loadnpy(conf.i.path .. a .. '/' .. s):transpose(1, 2):resize(wide, conf.i.frequency_width)
+        input = npy4th.loadnpy(conf.i.path .. a .. '/' .. s):transpose(1, 2)
+        w = math.floor((input:size(1) - wide) / 2)
 
-        x[i] = input
+        x[i] = input[{{w, w + wide - 1}, {}}]
         yt[i] = a
 
         i = i + 1
@@ -43,7 +44,7 @@ for a in paths.iterdirs(conf.i.path) do
 end
 
 -- TODO: Calculate this
-s = 254 * 512 -- wide * conf.i.frequency_width / 4 / 2 / 2
+s = 30 * 512 -- wide * conf.i.frequency_width / 4 / 2 / 2
 
 -- model
 model = nn.Sequential()
@@ -68,6 +69,25 @@ model:add(nn.LogSoftMax())
 
 criterion = nn.ClassNLLCriterion()
 
+
+-- validate
+function validate() 
+  function sample(path)
+    local v_input = npy4th.loadnpy(path):transpose(1, 2):resize(wide, conf.i.frequency_width)
+    local v_prob = model:forward(v_input)
+    local v_mt, v_mi = v_prob:max(1)
+
+    return v_mi[1]
+  end
+
+
+  print(
+    sample('spect/989787.LOFI.mp3.npy'), -- jamie
+    sample('spect/3322004.LOFI.mp3.npy'), -- move d
+    sample('spect/dixon.mp3.npy')
+  )
+end
+
 -- training
 for e = 1, conf.t.epochs do
     local permutation = torch.randperm(yt:size(1))
@@ -87,28 +107,8 @@ for e = 1, conf.t.epochs do
         model:updateParameters(0.01)
     end
     
+    validate()
     print("epoch: " .. e .. ", loss: " .. loss/conf.i.num_samples )
 end
 
 
--- prediction
-local v_path = 'spect/989787.LOFI.mp3.npy'
-local v_input = npy4th.loadnpy(v_path):transpose(1, 2):resize(wide, conf.i.frequency_width)
-local v_prob = model:forward(v_input)
-local v_mt, v_mi = v_prob:max(1)
-
-print(v_mi)
-
-v_path = 'spect/3322004.LOFI.mp3.npy'
-v_input = npy4th.loadnpy(v_path):transpose(1, 2):resize(wide, conf.i.frequency_width)
-v_prob = model:forward(v_input)
-v_mt, v_mi = v_prob:max(1)
-
-print(v_mi)
-
-v_path = 'spect/dixon.mp3.npy'
-v_input = npy4th.loadnpy(v_path):transpose(1, 2):resize(wide, conf.i.frequency_width)
-v_prob = model:forward(v_input)
-v_mt, v_mi = v_prob:max(1)
-
-print(v_mi)
