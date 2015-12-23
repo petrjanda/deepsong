594b30d4e77c6374ea7793d553024bc7ee50da4a
Petr Janda <petrjandacz@gmail.com>
2015-12-23 18:26:22 +0000

    no limits

--- ds.lua
+++ ds.lua
@@ -13,19 +13,17 @@ local function load(conf)
 
   i = 1
   for s in paths.iterfiles(conf.path) do
-    if i < 1300 then
-        input = npy4th.loadnpy(conf.path .. '/' .. s)
-        w = math.floor((input:size(1) - conf.width) / 2)
+    input = npy4th.loadnpy(conf.path .. '/' .. s)
+    w = math.floor((input:size(1) - conf.width) / 2)
 
-        label = tonumber(string.sub(s, 1, 1))
+    label = tonumber(string.sub(s, 1, 1))
 
-        x[i] = input[{{w+1, w + conf.width}, {}}]
-        yt[i] = label
+    x[i] = input[{{w+1, w + conf.width}, {}}]
+    yt[i] = label
 
-        classes = Set.union(classes, Set.new{label})
+    classes = Set.union(classes, Set.new{label})
 
-        i = i + 1
-    end
+    i = i + 1
   end
 
   return {count=count, x=x, yt=yt, classes=Set.keys(classes)}
